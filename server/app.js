985816681e6b9681660789093ffff1e44f878f3c
mmataja <mmataja92@gmail.com>
2020-01-26 19:14:51 +0100

    Change default port value

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const http = require('http');
 
 const { routes } = require('./api');
 
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(bodyParser.json());
@@ -11,6 +12,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', routes());
 
-http.createServer(app).listen(3000, () => {
-  console.log('Api listening on port 3000');
+http.createServer(app).listen(PORT, () => {
+  console.log(`Api listening on port ${PORT}`);
 })
