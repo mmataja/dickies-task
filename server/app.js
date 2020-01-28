797e972c500101420b7052453f21a94913fa487e
mmataja <mmataja92@gmail.com>
2020-01-28 23:08:19 +0100

    Add Access-Control-Allow-Origin for incoming requests

--- server/app.js
+++ server/app.js
@@ -9,6 +9,11 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use('/', routes());
 
