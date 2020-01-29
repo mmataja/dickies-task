888e9a226ecf9cd864e728e9b100eb982e9711df
mmataja <mmataja92@gmail.com>
2020-01-30 00:20:35 +0100

    Change constants path

--- server/api/routes/api/movies.js
+++ server/api/routes/api/movies.js
@@ -1,4 +1,4 @@
-const { paths } = require('../../../../common/constants');
+const { paths } = require('../../../constants');
 
 const { movies } = require('../../controllers');
 
---
+++ server/constants/paths/client.js
@@ -0,0 +1,3 @@
+module.exports.BASE = '/';
+module.exports.MOVIES = '/movies';
+module.exports.MOVIES_ID = '/movies/:id';
